3d7e1e1c40fb4bf94092d6fc4ecd77fa6a378ea5
Lyan_Nandyan <lyannandyan@gmail.com>
2024-11-19 04:38:20 +0700

    test

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ const app = express();
 // Aktifkan CORS dengan credentials
 app.use(
   cors({
-    origin: "http://localhost:5173", // URL frontend di lokal Anda
-    credentials: true, // Jika Anda menggunakan cookie atau header khusus
+    origin: 'http://localhost:5173', // Pastikan ini sesuai dengan alamat frontend Anda
+    credentials: true, // Jika Anda menggunakan cookies atau header khusus
   })
 );
 
